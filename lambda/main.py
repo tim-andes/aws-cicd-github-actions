4ccbd0ee3cae6b88aa5cb4d2df8f2099c404ed5a
TIMOTHY ANDES <tim.andes+git@gmail.com>
2024-02-29 10:44:49 -0700

    fix version

--- lambda/main.py
+++ lambda/main.py
@@ -1,9 +1,9 @@
 import os
 
 def handler(event, context):
+    version = os.environ.get("VERSION", "0.0")
     response_body = {
-            version = os.environ.get("VERSION", "0.0")
-            "message": "Hello World",
-            "version": version
-        }
-    return response_body
+        "message": "Hello World 👋",
+        "version": version,
+    }
+    return {"statusCode": 200, "body": response_body}
