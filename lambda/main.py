99bc04ef5ebd119c549573f5271c1d9041fefd25
TIMOTHY ANDES <tim.andes+git@gmail.com>
2024-02-29 09:50:29 -0700

    add GH actions yml, add new python lambda code

--- lambda/main.py
+++ lambda/main.py
@@ -1,6 +1,6 @@
 def handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": "Hello World"
-    }
-    
+    response_body = {
+            "message": "Hello World",
+            "version": "1.0.0"
+        }
+    return response_body
